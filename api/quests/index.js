347a8f2a0ea839e8f73adab072a08ce66fb365d6
sht27363838-glitch <sht27363838@gmail.com>
2025-10-22 15:46:03 +0900

    Update index.js

--- api/quests/index.js
+++ api/quests/index.js
@@ -1,20 +1,9 @@
 const { db } = require('../../_db.js');
-
-async function readBody(req) {
-  return new Promise((resolve) => {
-    let data = '';
-    req.on('data', chunk => { data += chunk; });
-    req.on('end', () => {
-      try { resolve(data ? JSON.parse(data) : {}); }
-      catch { resolve({}); }
-    });
-  });
-}
-
+async function readBody(req){ return await new Promise(r=>{ let d=''; req.on('data',c=>d+=c); req.on('end',()=>{ try{ r(JSON.parse(d||'{}')) }catch{ r({}) } }) }) }
 module.exports = async (req, res) => {
   try {
     if (req.method === 'GET') {
-      res.setHeader('content-type', 'application/json; charset=utf-8');
+      res.setHeader('content-type','application/json; charset=utf-8');
       return res.status(200).end(JSON.stringify(db.quests));
     }
     if (req.method === 'POST') {
@@ -22,7 +11,7 @@ module.exports = async (req, res) => {
       const id = `q_${Date.now()}`;
       const quest = { id, status:'planned', completed_at:null, ...body };
       db.quests.push(quest);
-      res.setHeader('content-type', 'application/json; charset=utf-8');
+      res.setHeader('content-type','application/json; charset=utf-8');
       return res.status(201).end(JSON.stringify(quest));
     }
     res.status(405).end('Method Not Allowed');
