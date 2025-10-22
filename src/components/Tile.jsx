8f44646701b64d02b0cd45163941b6e3d27cf90a
sht27363838-glitch <sht27363838@gmail.com>
2025-10-22 23:50:44 +0900

    Update Tile.jsx

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-
-export default function Tile({ icon, title, desc, href }) {
+export default function Tile({ icon:Icon, title, desc, href }) {
   return (
     <a className="tile" href={href}>
-      <div className="tile-icon">{icon}</div>
+      <div className="tile-icon">{Icon ? <Icon size={40} strokeWidth={1.5}/> : "□"}</div>
       <div className="tile-title">{title}</div>
       <div className="tile-desc">{desc}</div>
     </a>
