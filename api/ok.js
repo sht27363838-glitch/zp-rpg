3a4b08020965bfc7ae1bf55632df9c6ee0bd500a
sht27363838-glitch <sht27363838@gmail.com>
2025-10-23 22:55:57 +0900

    Update ok.js

--- api/ok.js
+++ api/ok.js
@@ -1,1 +1,5 @@
-module.exports = (_,res)=> res.status(200).json({ ok:true, from:'api/ok' });
+// /api/ok.js
+module.exports = (_, res) => {
+  res.setHeader('content-type','application/json; charset=utf-8');
+  res.status(200).end(JSON.stringify({ ok: true, from: 'api/ok', time: new Date().toISOString() }));
+};
